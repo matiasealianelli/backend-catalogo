d804686a115d01021b31d27c4b35d5d316e3c86f
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-11-02 13:30:28 -0300

    Agregue validaciones y encriptación de contraseñas en el modelo de usuario

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const User = new mongoose.Schema(
   {
@@ -6,19 +7,19 @@ const User = new mongoose.Schema(
       type: String,
       require: [true, "Name field is required"],
       minLength: 3,
-        maxLength: 20,
-        unique: true,
-        lowercase: true,
-        trim: true
+      maxLength: 20,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-   lastName: {
+    lastName: {
       type: String,
       require: [true, "LastName field is required"],
       minLength: 3,
-        maxLength: 20,
-        unique: true,
-        lowercase: true,
-        trim: true
+      maxLength: 20,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     email: {
       type: String,
@@ -26,15 +27,29 @@ const User = new mongoose.Schema(
       maxLength: 50,
       trim: true,
       lowercase: true,
-      match: /^\S+@\S+\.\S+$/
+      match: /^\S+@\S+\.\S+$/,
     },
     password: {
-      type: String, 
-      required: [true, "Password invalided"]
+      type: String,
+      required: [true, "Password invalided"],
     },
   },
-   {timeStamps:true}
-)
+  { timeStamps: true }
+);
 
+User.pre("save", function (next) {
+  bcrypt
+    .genSalt(10)
+    .then((salts) => {
+      bcrypt
+        .hash(this.password, salts)
+        .then((hash) => {
+          this.password = hash;
+          next();
+        })
+        .catch((error) => next(error));
+    })
+    .catch((error) => next(error));
+});
 
-export default mongoose.model("user", User)
+export default mongoose.model("user", User);
